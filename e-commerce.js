30d647a6dd85b894320dc20459c877db43a06a8e
Rachael Sikuku <rachaelsikuku033@gmail.com>
2025-03-17 11:32:27 +0300

    More javascript

--- e-commerce.js
+++ e-commerce.js
@@ -18,3 +18,31 @@ closeCart.addEventListener("click",()=>{
 
 
 })
+let changeToImage1= document.querySelector(".small1")
+changeToImage1.addEventListener("click",()=>{
+    document.querySelector(".big-image1").style.display="block"
+     document.querySelector(".big-image2").style.display="none"
+      document.querySelector(".big-image3").style.display="none"
+       document.querySelector(".big-image4").style.display="none"
+})
+let changeToImage2= document.querySelector(".small2")
+changeToImage2.addEventListener("click",()=>{
+    document.querySelector(".big-image1").style.display="none"
+     document.querySelector(".big-image2").style.display="block"
+      document.querySelector(".big-image3").style.display="none"
+       document.querySelector(".big-image4").style.display="none"
+})
+let changeToImage3= document.querySelector(".small3")
+changeToImage3.addEventListener("click",()=>{
+    document.querySelector(".big-image1").style.display="none"
+     document.querySelector(".big-image2").style.display="none"
+      document.querySelector(".big-image3").style.display="block"
+       document.querySelector(".big-image4").style.display="none"
+})
+let changeToImage4= document.querySelector(".small4")
+changeToImage4.addEventListener("click",()=>{
+    document.querySelector(".big-image1").style.display="none"
+     document.querySelector(".big-image2").style.display="none"
+      document.querySelector(".big-image3").style.display="none"
+       document.querySelector(".big-image4").style.display="block"
+})
